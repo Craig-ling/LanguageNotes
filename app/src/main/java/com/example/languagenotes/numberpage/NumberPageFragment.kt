2fa4d8b92a2ba135bf1642925be530ed6e82fa3a
Craig-ling <craigboucher9@gmail.com>
2021-03-19 16:06:09 -0400

    Implemented two digit random number generation with ImageViews.

--- app/src/main/java/com/example/languagenotes/numberpage/NumberPageFragment.kt
+++ app/src/main/java/com/example/languagenotes/numberpage/NumberPageFragment.kt
@@ -1,21 +1,27 @@
 package com.example.languagenotes.numberpage
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.languagenotes.R
 import com.example.languagenotes.databinding.FragmentNumberPageBinding
 import com.example.languagenotes.languages.German
 
 class NumberPageFragment : Fragment() {
+
+    private lateinit var binding: FragmentNumberPageBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
 
-        val binding: FragmentNumberPageBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_number_page, container, false)
 
         val arguments = NumberPageFragmentArgs.fromBundle(requireArguments())
@@ -34,7 +40,33 @@ class NumberPageFragment : Fragment() {
 
         numberPageViewModel.setNumber(12)
 
+        numberPageViewModel.genNumberBegin.observe(viewLifecycleOwner, Observer { itBool ->
+            if (itBool) {
+                val numList = numberPageViewModel.generateNumbers()
+                Log.i("NumberPageFragment", "$numList")
+                setDigitImage(numList[0], binding.firstDigit)
+                setDigitImage(numList[1], binding.secondDigit)
+                numberPageViewModel.endGenerateNumber()
+            }
+        })
 
         return binding.root
     }
+
+    private fun setDigitImage(num: Int, imgView: ImageView) {
+        Log.i("NumberPageFragment", "$num")
+        when(num) {
+            0 -> imgView.setImageResource(R.drawable.zero)
+            1 -> imgView.setImageResource(R.drawable.one)
+            2 -> imgView.setImageResource(R.drawable.two)
+            3 -> imgView.setImageResource(R.drawable.three)
+            4 -> imgView.setImageResource(R.drawable.four)
+            5 -> imgView.setImageResource(R.drawable.five)
+            6 -> imgView.setImageResource(R.drawable.six)
+            7 -> imgView.setImageResource(R.drawable.seven)
+            8 -> imgView.setImageResource(R.drawable.eight)
+            9 -> imgView.setImageResource(R.drawable.nine)
+        }
+    }
+
 }
--- app/src/main/java/com/example/languagenotes/numberpage/NumberPageViewModel.kt
+++ app/src/main/java/com/example/languagenotes/numberpage/NumberPageViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.languagenotes.languages.Language
+import kotlin.random.Random
 
 class NumberPageViewModel(inLang: Language): ViewModel() {
 
@@ -13,9 +14,24 @@ class NumberPageViewModel(inLang: Language): ViewModel() {
     val numberText: LiveData<String>
         get() = _numberText
 
+    private val _genNumberBegin = MutableLiveData<Boolean>()
+    val genNumberBegin: LiveData<Boolean>
+        get() = _genNumberBegin
+
     fun setNumber(num: Int) {
         _numberText.value = numberMap[num]
     }
 
+    fun beginGenerateNumber() {
+        _genNumberBegin.value = true
+    }
+
+    fun endGenerateNumber() {
+        _genNumberBegin.value = false
+    }
+
+    fun generateNumbers() : List<Int> {
+        return listOf(Random.nextInt(0, 10), Random.nextInt(0,10))
+    }
 
 }
